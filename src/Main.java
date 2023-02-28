45cfe5eb4f42bd36c23b5a98137b3af9b2e0bd4e
taylorj8 <taylorj8@tcd.ie>
2023-02-28 22:36:37 +0000

    blacklist implemented

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import javax.net.ssl.KeyManagerFactory;
+import java.io.FileOutputStream;
 import java.net.*;
+import java.security.KeyStore;
 
 public class Main {
 
-    public static void main(String[] args) throws UnknownHostException, MalformedURLException {
-        InetAddress test = InetAddress.getByName("www.google.com");
-        URL url = new URL("HTTP://www.google.com");
-        System.out.println(url);
+    public static void main(String[] args) throws Exception
+    {
+
     }
 }
