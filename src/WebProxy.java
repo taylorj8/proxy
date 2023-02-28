903a921fb9e4f72ef1fc9264b8fc4d17b4379ddb
taylorj8 <taylorj8@tcd.ie>
2023-02-28 22:17:33 +0000

    started on blacklist

--- src/WebProxy.java
+++ src/WebProxy.java
@@ -1,6 +1,10 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,30 +19,26 @@ class WebProxy {
     int remotePort;
     static Pattern pattern;
     URLConnection connection;
+    ArrayList<String> blacklist;
 
     WebProxy(int localPort, int remotePort) {
 
         this.remotePort = remotePort;
-        hostName = "Web Proxy";
+        hostName = "WebProxy";
         latch = new CountDownLatch(1);
         connection = null;
-        startListener();
+        blacklist = new ArrayList<>();
+
+        listener = new Listener();
+        listener.setDaemon(true);
 
         pattern = Pattern.compile("(?<=Host: ).*(?=\\R)");
 
         try {
             socket = new ServerSocket(localPort);
 //            System.out.println(socket.getLocalSocketAddress());
-            listener.go();
         }
         catch(java.lang.Exception e) {e.printStackTrace();}
-    }
-
-    private void startListener()
-    {
-        listener = new Listener();
-        listener.setDaemon(true);
-        listener.start();
     }
 
     static class RequestHandler extends Thread {
@@ -67,7 +67,7 @@ class WebProxy {
 
                     Matcher m = pattern.matcher(strRequest);
                     String hostName = "";
-                    int port = 443;
+                    int port = 80;
                     if(m.find())
                     {
                         String[] hostPort = m.group().split(":");
@@ -77,7 +77,15 @@ class WebProxy {
                         {
                             port = Integer.parseInt(hostPort[1]);
                         }
+                        else if(strRequest.contains("https://"))
+                        {
+                            port = 443;
+                        }
                     }
+
+//                    SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
+//                    SSLSocket server = (SSLSocket)factory.createSocket(hostName, port);
+//                    server.startHandshake();
 
                     Socket server = new Socket(hostName, port);
                     OutputStream toServer = server.getOutputStream();
@@ -120,17 +128,11 @@ class WebProxy {
      */
     class Listener extends Thread {
 
-        // Tell the listener that the socket has been initialized
-        public void go() {
-            latch.countDown();
-        }
-
         // Listen for incoming packets and inform receivers
         @Override
         public void run() {
 
             try {
-                latch.await();
                 // Endless loop: attempt to receive packet, notify receivers, etc
                 while(true)
                 {
@@ -141,41 +143,58 @@ class WebProxy {
                         new RequestHandler(client).start();
                     }
                     catch(Exception ignored) {}
-                    finally
-                    {
-//                        try
-//                        {
-//                            if(client != null)
-//                                client.close();
-//                        }
-//                        catch(Exception ignored) {}
-                    }
                 }
             }
-            catch (Exception e) {e.printStackTrace();}
+            catch(Exception e) {e.printStackTrace();}
         }
     }
 
-    // function for passing request from client to server or reply from server to client
-    private void pass(byte[] re, InputStream from, OutputStream to)
-    {
-        int bytesRead;
-        try
+    static class Blocker extends Thread {
+
+        ArrayList<String> blacklist;
+        Blocker(ArrayList<String> blacklist)
         {
-            while((bytesRead = from.read(re)) != -1)
+            this.blacklist = blacklist;
+        }
+
+        @Override
+        public void run()
+        {
+            Scanner input = new Scanner(System.in);
+            while(true)
             {
-                to.write(re, 0, bytesRead);
-                to.flush();
+                String url = input.nextLine();
+                System.out.printf("Are you sure you want to block \"%s\"? (y/n):\n", url);
+
+                boolean valid = false;
+                while(!valid)
+                {
+                    if(input.nextLine().equalsIgnoreCase("y"))
+                    {
+                        blacklist.add(url);
+                        System.out.println("URL blocked.");
+                        valid = true;
+                    }
+                    else if(input.nextLine().equalsIgnoreCase("n"))
+                    {
+                        System.out.println("URL not blocked.");
+                        valid = true;
+                    }
+                    else
+                    {
+                        System.out.println("Enter y or n:");
+                    }
+                }
             }
-            to.close();
-
-        } catch(IOException ignored) {}
+        }
     }
-
 
     private synchronized void start() throws InterruptedException
     {
-        System.out.println("Proxy Server running");
+        listener.start();
+        (new Blocker(blacklist)).start();
+
+        System.out.println("Proxy Server running. A URL can be entered at any time to block it.");
         this.wait();
     }
 
