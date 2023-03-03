36c6dda9d3efcbab3f1101c64c2ab15d85116bb4
taylorj8 <taylorj8@tcd.ie>
2023-03-03 13:48:03 +0000

    added show blacklist command

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,29 @@ public class Main {
 
     public static void main(String[] args) throws Exception
     {
+        Test t1 = new Test(1);
+        Test t2 = new Test(2);
+        Test t3 = new Test(3);
 
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+
+    static class Test extends Thread {
+
+        int test;
+
+        Test(int i)
+        {
+            this.test = i;
+        }
+
+        @Override
+        public void run()
+        {
+            System.out.println(test);
+        }
     }
 }
